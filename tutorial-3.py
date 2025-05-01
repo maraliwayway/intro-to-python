b6e82d0fe0f529476a5528fa245a1e23c61ffc15
Mara Liwayway David <davidmaraliwayway@gmail.com>
2025-05-01 02:56:13 -0700

    chore: add tutorial-3.py

---
+++ tutorial-3.py
@@ -0,0 +1,16 @@
+# tutorial-3.py
+
+# 0's are the default game map
+# Square braces for lists so we can change the elements
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],]
+
+# Proof it's a list
+print(type(game))
+
+# Display
+for row in game:
+    print(row)
+
+
